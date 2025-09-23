dc332e06322a7de3d30a3a77cf90c6400189e4ff
Rory McLeod <mcleodnw@gmail.com>
2025-09-23 16:23:50 -0700

    insertMenu

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@ function updatemenu() {
     document.getElementById('menu').style.borderRadius = '10px';
   }
 }
+
+function handleMenuText(html) {
+    const menu_container = document.getElementById('menu_container');
+    menu_container.innerHTML = html;
+}
+
+function insertMenu() {
+    try {
+        const html_file = `/content/menu.html`;
+        fetch(html_file)
+        .then(response => response.text())
+        .then(html => handleMenuText(html))
+        .catch(error => console.error(error));
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+
+window.onload = () => {
+  insertMenu();
+};
