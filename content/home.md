a4ecc2e49ba7ec8ef07369385d7c042af05bef6b
Rory McLeod <mcleodnw@gmail.com>
2025-09-23 17:04:24 -0700

    dynamic pages.

---
+++ content/home.md
@@ -0,0 +1,28 @@
+# What is Eliot?
+
+<figure>
+    <img src="img/firelight_slide.jpg" alt="Songs at Firelight" />
+    <figcaption>Songs at Firelight</figcaption>
+</figure>
+
+It's a <b>summer camp</b> for people of all ages, filled with laughter, 
+singalongs, swimming in the lagoon, talent shows, and group meals in the dining hall.
+
+It's a <b>retreat</b>, with thought-provoking morning programs followed by small group 
+discussions, and additional opportunities to explore social justice issues.
+
+It's an opportunity for <b>spiritual connection</b>, with worship services each evening.
+
+It's a chance to express <b>creativity</b> - explore music in choir, rock band and 
+coffeehouse performances, learn a new visual art, dance, or tie-dye.
+
+It's a safe space for <b>personal growth</b>, from the profound to the silly.
+
+It's a time to enjoy <b>nature-walk</b> in the woods, watch the sunset, listen 
+to the birds, and look for shells on the shore of Hood Canal.
+
+It's a <b>]multi-generational community</b> of people who find their happy place at Seabeck Conference Center.
+
+<b>Eliot is all of these things and more!</b>
+
+Learn more in our <a href="">Introduction to Camp</a>. Or check out the images below.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var converter = new showdown.Converter();
+
 function updatemenu() {
   if (document.getElementById('responsive-menu').checked == true) {
     document.getElementById('menu').style.borderBottomRightRadius = '0';
@@ -8,14 +10,14 @@ function updatemenu() {
 }
 
 function handleMenuText(html) {
-    const menu_container = document.getElementById('menu_container');
-    menu_container.innerHTML = html;
+    const container = document.getElementById('menu_container');
+    container.innerHTML = html;
 }
 
 function insertMenu() {
     try {
-        const html_file = `/content/menu.html`;
-        fetch(html_file)
+        const link = `/content/menu.html`;
+        fetch(link)
         .then(response => response.text())
         .then(html => handleMenuText(html))
         .catch(error => console.error(error));
@@ -25,7 +27,36 @@ function insertMenu() {
     }
 }
 
+function handlePage(md) {
+    console.log("got someting");
+    const container = document.getElementById('content_container');
+
+    const html = converter.makeHtml(md);
+    container.innerHTML = html;
+}
+
+function insertContent() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let page = urlParams.get('page');
+    if (page === null) {
+        page = 'home';
+    }
+
+    try {
+        const link = `content/${page}.md`;
+        fetch(link)
+        .then(response => response.text())
+        .then(md => handlePage(md))
+        .catch(error => console.error(error));
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return;
+}
 
 window.onload = () => {
   insertMenu();
+  insertContent();
 };
